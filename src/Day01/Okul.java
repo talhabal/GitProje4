33c09c9df16c63dd419534716e9889fd0b7a6024
talhabal <68268759+talhabal@users.noreply.github.com>
2023-01-26 19:22:45 +0300

    Student clasına bi Yes 222222222 daha eklendiiiii

---
+++ src/Day01/Okul.java
@@ -0,0 +1,8 @@
+package Day01;
+
+public class Okul {
+    public static void main(String[] args) {
+        System.out.println("Yesssss");
+        System.out.println("Yesssss  22222222");
+    }
+}
