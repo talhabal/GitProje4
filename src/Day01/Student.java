525a2d9c879fb9803f871ec3ff37c535e4083f5a
talhabal <68268759+talhabal@users.noreply.github.com>
2023-01-26 19:35:07 +0300

    Student clasına bi yeniikler geldii  eklendiiiii

--- src/Day01/Student.java
+++ src/Day01/Student.java
@@ -4,5 +4,7 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Helloooo!");
         System.out.println("Selammmm!");
+        System.out.println("Yeniliklerer");
+        System.out.println("Yeniliklerer geldiiiii");
     }
 }
